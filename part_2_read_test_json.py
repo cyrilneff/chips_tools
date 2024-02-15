621bf8bee209c4b56ba2eb046da29077fdeaf067
cyrilneff <clneff@andrew.cmu.edu>
2024-02-15 01:00:07 -0500

    Working with JSON string creation commit
    Added the code to "part_2_read_test_json.py", which reads the json files string for the game library. Also fixed an error in the JSON file.

--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -8,11 +8,21 @@ def make_game_library_from_json( json_data ):
 
     ### Begin Add Code Here ###
     #Loop through the json_data
+    gameList = json_data["games"]
+    for game in gameList:
+        title = game["title"]
+        year = game["year"]
+        platform = game["platform"]
+
         #Create a new Game object from the json_data by reading
         #  title
         #  year
         #  platform (which requires reading name and launch_year)
         #Add that Game object to the game_library
+        gamePlatform = test_data.Platform(platform["name"], platform["launch year"])
+        newGame = test_data.Game(title, gamePlatform, year)
+        game_library.add_game(newGame)
+
     ### End Add Code Here ###
 
     return game_library
@@ -24,6 +34,10 @@ input_json_file = "data/test_data.json"
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
 #Use the json module to load the data from the file
+with open(input_json_file, "r") as reader:
+    game_json_data = json.load(reader)
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
+game_library = make_game_library_from_json(game_json_data)
 #Print out the resulting GameLibrary data using print()
+print(game_library)
 ### End Add Code Here ###
